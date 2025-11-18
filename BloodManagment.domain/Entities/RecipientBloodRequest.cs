995e2d1775dbbc202108d1bbf363f5b5dde426a6
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:31:51 +0200

    Add RecipientBloodRequest Entity

---
+++ BloodManagment.domain/Entities/RecipientBloodRequest.cs
@@ -0,0 +1,32 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class RecipientBloodRequest : BaseEntity
+    {
+
+
+        public BloodGroup BloodGroup { get; set; }
+
+        public int UnitsNeeded { get; set; }      // Number of units requested
+
+        // Request Details
+        public DateTime RequestDate { get; set; } = DateTime.UtcNow;
+
+        public string HospitalName { get; set; }
+        public string Diagnosis { get; set; }      // Thalassemia, Surgery, Accident…
+
+        // Status
+        public RequestStatus Status { get; set; } = RequestStatus.Pending;
+
+        // Recipient Info
+        public int RecipientId { get; set; }      // FK → User
+        public Rescipient Recipient { get; set; }
+
+        // Optional Notes
+        public string Notes { get; set; }
+
+
+    }
+
+}
