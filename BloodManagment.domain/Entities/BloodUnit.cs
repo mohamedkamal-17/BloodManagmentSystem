bd67771e8ddbd62758901a4bea7c9a9cf3a84d54
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:24:55 +0200

    Add BloodUnit Entity

---
+++ BloodManagment.domain/Entities/BloodUnit.cs
@@ -0,0 +1,18 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class BloodUnit : BaseEntity
+    {
+
+
+        public BloodGroup BloodGroup { get; set; }
+
+        public DateTime DonationDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
+        public BloodUnitStatus Status { get; set; } = BloodUnitStatus.Available;
+
+        public string StorageLocation { get; set; }
+    }
+}
