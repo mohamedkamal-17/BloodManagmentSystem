adfe80118f09a7ac3f27dc128835dcbe61602d65
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:30:48 +0200

    Add LabTechnician Entity

---
+++ BloodManagment.domain/Entities/LabTechnician.cs
@@ -0,0 +1,17 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class LabTechnician : BaseEntity
+    {
+        public DateTime HireDate { get; set; }
+
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public int HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+
+
+    }
+}
