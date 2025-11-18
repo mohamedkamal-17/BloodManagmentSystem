0ef63f0261a72e8c959e45ed6f7e8110575ac80d
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:29:50 +0200

    Add Hospital Entity

---
+++ BloodManagment.domain/Entities/Hospital.cs
@@ -0,0 +1,17 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class Hospital : BaseEntity
+    {
+        public string HospitalName { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+
+        public ICollection<LabTechnician> LabTechnicians { get; set; } = new HashSet<LabTechnician>();
+        public ICollection<BloodInventory> BloodInventories { get; set; } = new HashSet<BloodInventory>();
+        public ICollection<BloodRequest> BloodRequests { get; set; } = new HashSet<BloodRequest>();
+
+    }
+}
