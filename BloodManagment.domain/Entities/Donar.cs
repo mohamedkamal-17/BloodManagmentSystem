b18e92a88e3d73a412204e48b655438f5494262e
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:27:57 +0200

    Add  Donar Entity

---
+++ BloodManagment.domain/Entities/Donar.cs
@@ -0,0 +1,26 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public class Donar
+    {
+        public int Id { get; set; }
+
+        public string DonarCode { get; set; }
+        public DateTime NextDonationDate { get; set; }
+
+        public DateTime LastDonationDate { get; set; }
+
+        public short DonationCount { get; set; } = default;
+
+        public bool IsEilgibleToDonate { get; set; } = default
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public ICollection<ThalassemiaPatientDonorAssignement> thalassemiaPatientDonorAssignements { get; set; } = new HashSet<ThalassemiaPatientDonorAssignement>();
+
+
+
+
+
+    }
+}
