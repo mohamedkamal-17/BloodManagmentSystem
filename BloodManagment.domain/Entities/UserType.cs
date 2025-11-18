10ce86425b6b03c1dffe5cf70762753a9c40aa2e
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:34:59 +0200

    Add  UserType Enum

---
+++ BloodManagment.domain/Entities/UserType.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public enum UserType
+    {
+        Donor = 1,
+        Patient = 2,
+        AnemiaPatient = 3,
+        Hospital = 4,
+        BloodBank = 5
+    }
+
+}
