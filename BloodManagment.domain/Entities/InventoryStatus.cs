316eae30140bf2500366452713dc9eac70fea441
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:30:13 +0200

    Add InventoryStatus Enum

---
+++ BloodManagment.domain/Entities/InventoryStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public enum InventoryStatus
+    {
+        Normal,         // المخزون في مستوى آمن
+        Low,            // المخزون منخفض ويحتاج تنبيه
+        Critical,       // المخزون خطير ويحتاج توفير عاجل
+        OutOfStock,     // المخزون نفد تمامًا
+        Overloaded      // مخزون زائد عن الحاجة 
+    }
+
+}
