8afec46ee536cde1ec837c4af85ccf2a683f4b7b
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:28:51 +0200

    Add Gender Enum

---
+++ BloodManagment.domain/Entities/Gender.cs
@@ -0,0 +1,12 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other,       // لو عايز تدعم خيارات غير ثنائية
+        Unknown      // لو لم يتم تحديد الجنس بعد
+    }
+
+
+}
