2325c14fb827f1ececf8312ac5c8bf753ac157cb
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:20:31 +0200

    Add BloodInventoty Entity

---
+++ BloodManagment.domain/Entities/BloodInventory.cs
@@ -0,0 +1,19 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class BloodInventory : BaseEntity
+    {
+
+        public int Quantity { get; set; }
+
+        public BloodGroup BloodGroup { get; set; }
+
+
+        public InventoryStatus Status { get; set; }
+
+        public int HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+
+    }
+}
