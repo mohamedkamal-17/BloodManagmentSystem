fe0c35f768ac4f951df90443d145b157299504c9
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:34:05 +0200

    Add ThalassemiaPatient Entity

---
+++ BloodManagment.domain/Entities/ThalassemiaPatient.cs
@@ -0,0 +1,19 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public class ThalassemiaPatient
+    {
+        public DateTime DiagnosisDate { get; set; }
+        public DateTime LastTransfusionDate { get; set; }
+        public DateTime NextTransfusionDate { get; set; }
+
+        public int HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public ICollection<ThalassemiaPatientDonorAssignement> thalassemiaPatientDonorAssignements { get; set; } = new HashSet<ThalassemiaPatientDonorAssignement>();
+
+    }
+}
