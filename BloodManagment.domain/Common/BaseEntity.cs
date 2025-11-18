281c50405c1941d7917547eef56b2fa3c9ab26fb
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:17:35 +0200

    Add BaseEntity to Comman foldar

---
+++ BloodManagment.domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace BloodManagment.domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        // Audit Fields
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+
+        // Soft Delete
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+        public string? DeletedBy { get; set; }
+
+    }
+}
