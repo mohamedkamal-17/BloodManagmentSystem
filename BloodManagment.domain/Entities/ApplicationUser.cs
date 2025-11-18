072e2a30a6ce1fb8c00ab26a58b1ce44981095b1
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:19:17 +0200

    Add AplicationUser Entity With IdentityUser

---
+++ BloodManagment.domain/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BloodManagment.domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Additional properties can be added here as needed    
+        public string FullName { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
