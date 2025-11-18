a53ed9667310936e4aab4b84e4e569c2af25d39a
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:33:41 +0200

    Add Rescipient Entity

---
+++ BloodManagment.domain/Entities/Rescipient.cs
@@ -0,0 +1,14 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public class Rescipient
+    {
+        public int Id { get; set; }
+
+        public string RescipientCode { get; set; }
+        public Gender Gender { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+    }
+}
