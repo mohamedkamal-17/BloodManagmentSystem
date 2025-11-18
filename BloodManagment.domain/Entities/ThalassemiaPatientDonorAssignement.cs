ace2ced2d35a7bafdc6fe969a20fbeb33701831d
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:34:24 +0200

    Add ThalassemiaPatientDonorAssignement Entity

---
+++ BloodManagment.domain/Entities/ThalassemiaPatientDonorAssignement.cs
@@ -0,0 +1,15 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class ThalassemiaPatientDonorAssignement : BaseEntity
+    {
+
+
+        public int ThalassemiaPatientId { get; set; }
+        public ThalassemiaPatient ThalassemiaPatient { get; set; }
+
+        public int DonarId { get; set; }
+        public Donar Donar { get; set; }
+    }
+}
