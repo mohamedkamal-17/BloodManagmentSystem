43b2e2ac6a7c42bd89e095090b69ec39ffb32c5a
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:31:08 +0200

    Add Notification Entity

---
+++ BloodManagment.domain/Entities/Notification.cs
@@ -0,0 +1,38 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        // Notification Content
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+
+        // Receiver (User who will see the notification)
+        public string? RescipientId { get; set; }
+        public Rescipient? User { get; set; }
+
+        public string? DonarId { get; set; }
+        public Donar? Donar { get; set; }
+
+        public string ThalassemiaPatientId { get; set; }
+        public ThalassemiaPatient? ThalassemiaPatient { get; set; }
+
+        // Optional: Link to request
+        public int? RequestId { get; set; }
+        public RecipientBloodRequest Request { get; set; }
+
+        public int? DonationRequestId { get; set; }
+        public DonationRequest? DonationRequest { get; set; }
+        // Type (Info, Warning, Success, Urgent...)
+        public NotificationType Type { get; set; }
+
+        // Status
+        public bool IsRead { get; set; } = false;
+
+        // Time
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+
+}
