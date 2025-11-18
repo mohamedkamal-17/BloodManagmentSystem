c49d60d3d9d6fb678f24b35060ce230f0bfd2862
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:28:16 +0200

    Add  DonationHistory Entity

---
+++ BloodManagment.domain/Entities/DonationHistory.cs
@@ -0,0 +1,30 @@
+﻿namespace BloodManagment.domain.Entities
+{
+
+
+    public class DonationHistory
+    {
+        public int Id { get; set; }   // Primary Key
+
+        public BloodGroup BloodGroup { get; set; }
+
+        public int UnitsDonated { get; set; }   // Number of units donated
+
+        // Dates & Tracking
+        public DateTime DonationDate { get; set; } = DateTime.Now;
+
+        public RequestStatus status { get; set; }  // Status of the donation record
+
+        public string DonationLocation { get; set; }  // Hospital / Center name
+        public string Notes { get; set; }             // Optional notes
+
+        // Donor Info
+        public int DonorId { get; set; }       // FK to User
+        public Donar Donor { get; set; }
+
+
+
+
+    }
+
+}
