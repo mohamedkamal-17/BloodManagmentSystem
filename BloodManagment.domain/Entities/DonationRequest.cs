e8c4d2611148c15e182b572f849cb29b521b0c1c
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:28:30 +0200

    Add DonationRequest Entity

---
+++ BloodManagment.domain/Entities/DonationRequest.cs
@@ -0,0 +1,21 @@
+﻿using BloodManagment.domain.Common;
+
+namespace BloodManagment.domain.Entities
+{
+    public class DonationRequest : BaseEntity
+    {
+        public string RequestCode { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime PreferredDonationDate { get; set; }
+
+
+        public RequestStatus Statu { get; set; } = RequestStatus.Pending;
+
+        public int HealthConditionId { get; set; }
+        public HealthCondition HealthCondition { get; set; }
+        public int DonarId { get; set; }
+        public Donar Donar { get; set; }
+
+
+    }
+}
