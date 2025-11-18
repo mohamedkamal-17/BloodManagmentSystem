a7a24e69a1023b4b3184597cecb7539dae31e426
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:25:37 +0200

    Add BloodUnitStatus Enum

---
+++ BloodManagment.domain/Entities/BloodUnitStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public enum BloodUnitStatus
+    {
+        Available,      // الوحدة صالحة وموجودة في المخزون
+        Reserved,       // الوحدة محجوزة لطلب مريض
+        Used,           // تم استخدامها في تبرع لمريض
+        Expired,        // انتهت صلاحيتها
+        Damaged,        // غير صالحة بسبب تلف أو خطأ تخزين
+        InTransit       // في طريقها من/إلى مركز أو مستشفى
+    }
+
+}
