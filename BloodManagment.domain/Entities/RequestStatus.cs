8c642ea29ed1197cf1b56aab6131133f111948fd
Mohamed kamal <winc84327@gmail.com>
2025-11-19 00:33:08 +0200

    Add RequestStatus Enum

---
+++ BloodManagment.domain/Entities/RequestStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace BloodManagment.domain.Entities
+{
+    public enum RequestStatus
+    {
+        Pending,        // Request has been created but not yet processed
+        Approved,       // Request has been approved
+        Rejected,       // Request has been rejected
+        Completed,      // Blood has been successfully provided
+        Cancelled       // Request was cancelled by the user or admin
+    }
+}
