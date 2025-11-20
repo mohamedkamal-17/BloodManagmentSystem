a026a7ad26a3960c6d5923c192d628da7fb55e7c
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:11:23 +0200

    Configur BloodRequest Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/BloodRequestConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class BloodRequestConfiguration : IEntityTypeConfiguration<BloodRequest>
+    {
+        public void Configure(EntityTypeBuilder<BloodRequest> builder)
+        {
+            builder.Property(x => x.Reason).HasMaxLength(300);
+            builder.Property(x => x.BloodGroup).HasConversion<string>();
+            builder.Property(x => x.Status).HasConversion<string>();
+
+            builder.HasOne(x => x.Hospital)
+                .WithMany(x => x.BloodRequests)
+                .HasForeignKey(x => x.HospitalId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Rescipient)
+                .WithMany(x => x.BloodRequests)
+                .HasForeignKey(x => x.RescipientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.LabTechnician)
+                .WithMany(x => x.BloodRequests)
+                .HasForeignKey(x => x.LabTechnicianId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
