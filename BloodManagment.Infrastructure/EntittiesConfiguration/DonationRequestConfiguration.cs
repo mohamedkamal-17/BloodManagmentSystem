4c1e52643ebf880308a86fc9180096d75b74d14a
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:40:54 +0200

    Configur DonationRequest Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/DonationRequestConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class DonationRequestConfiguration : IEntityTypeConfiguration<DonationRequest>
+    {
+        public void Configure(EntityTypeBuilder<DonationRequest> builder)
+        {
+            builder.Property(x => x.RequestCode).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Statu).HasConversion<string>();
+
+            builder.HasOne(x => x.HealthCondition)
+                .WithMany(x => x.DonationRequests)
+                .HasForeignKey(x => x.HealthConditionId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Donar)
+                .WithMany()
+                .HasForeignKey(x => x.DonarId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+
+}
