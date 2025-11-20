512aa8d20b76255e29d636d669ee0dcc5cce0a55
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:42:02 +0200

    Confuger LabTechnician Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/LabTechnicianConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class LabTechnicianConfiguration : IEntityTypeConfiguration<LabTechnician>
+    {
+        public void Configure(EntityTypeBuilder<LabTechnician> builder)
+        {
+            builder.Property(x => x.HireDate).IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Hospital)
+                .WithMany(x => x.LabTechnicians)
+                .HasForeignKey(x => x.HospitalId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+}
