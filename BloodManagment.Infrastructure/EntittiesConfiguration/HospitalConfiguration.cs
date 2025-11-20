6626f696b24aac913d7eb299c557e1a6e21a5954
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:41:36 +0200

    Configur Hospital Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/HospitalConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class HospitalConfiguration : IEntityTypeConfiguration<Hospital>
+    {
+        public void Configure(EntityTypeBuilder<Hospital> builder)
+        {
+            builder.Property(x => x.HospitalName).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Address).HasMaxLength(500);
+            builder.Property(x => x.ContactNumber).HasMaxLength(50);
+
+            builder.HasMany(x => x.LabTechnicians)
+                .WithOne(x => x.Hospital)
+                .HasForeignKey(x => x.HospitalId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.BloodInventories)
+                .WithOne(x => x.Hospital)
+                .HasForeignKey(x => x.HospitalId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.BloodRequests)
+                .WithOne(x => x.Hospital)
+                .HasForeignKey(x => x.HospitalId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+}
