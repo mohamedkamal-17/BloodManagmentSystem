30c8b7d46a220a5cb535a4ead43eb30e19bc89eb
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:41:14 +0200

    Configur HealthCondition Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/HealthConditionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class HealthConditionConfiguration : IEntityTypeConfiguration<HealthCondition>
+    {
+        public void Configure(EntityTypeBuilder<HealthCondition> builder)
+        {
+            builder.HasMany(x => x.DonationRequests)
+                .WithOne(x => x.HealthCondition)
+                .HasForeignKey(x => x.HealthConditionId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
