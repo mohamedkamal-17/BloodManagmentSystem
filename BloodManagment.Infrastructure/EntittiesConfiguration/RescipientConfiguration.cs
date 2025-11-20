04f4db0f45aab885640a54cd792f782fca1ca545
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:49:58 +0200

    Configur Rescipient Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/RescipientConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class RescipientConfiguration : IEntityTypeConfiguration<Rescipient>
+    {
+        public void Configure(EntityTypeBuilder<Rescipient> builder)
+        {
+            builder.Property(x => x.RescipientCode).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Gender).HasConversion<string>().IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+}
