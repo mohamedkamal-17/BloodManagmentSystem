5ad0cf88ca7fc7ef49db9472ab711d54241e04bf
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:40:14 +0200

    Configur DonationHistory Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/DonationHistoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class DonationHistoryConfiguration : IEntityTypeConfiguration<DonationHistory>
+    {
+        public void Configure(EntityTypeBuilder<DonationHistory> builder)
+        {
+            builder.Property(x => x.BloodGroup).HasConversion<string>();
+            builder.Property(x => x.DonationLocation).HasMaxLength(200);
+            builder.Property(x => x.Notes).HasMaxLength(500);
+            builder.Property(x => x.status).HasConversion<string>();
+
+            builder.HasOne(x => x.Donor)
+                .WithMany()
+                .HasForeignKey(x => x.DonorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+
+}
