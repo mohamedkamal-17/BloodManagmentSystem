6a5b8df0a3e85de696184411bd2628ecc7e7e913
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:48:11 +0200

    Configur RecipientBloodRequest Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/RecipientBloodRequestConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class RecipientBloodRequestConfiguration : IEntityTypeConfiguration<RecipientBloodRequestHistory>
+    {
+        public void Configure(EntityTypeBuilder<RecipientBloodRequestHistory> builder)
+        {
+            builder.Property(x => x.HospitalName).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Diagnosis).HasMaxLength(200);
+            builder.Property(x => x.Status).HasConversion<string>();
+            builder.Property(x => x.Notes).HasMaxLength(500);
+
+            builder.HasOne(x => x.Recipient)
+                .WithMany()
+                .HasForeignKey(x => x.RecipientId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
