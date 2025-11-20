1b46293c68df9b27e7861030774ac1f9e3be7625
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:50:27 +0200

    Configur  ThalassemiaPatientDonorAssignement Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/ThalassemiaPatientDonorAssignementConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class ThalassemiaPatientDonorAssignementConfiguration : IEntityTypeConfiguration<ThalassemiaPatientDonorAssignement>
+    {
+        public void Configure(EntityTypeBuilder<ThalassemiaPatientDonorAssignement> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.ThalassemiaPatient)
+                .WithMany(x => x.thalassemiaPatientDonorAssignements)
+                .HasForeignKey(x => x.ThalassemiaPatientId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Donar)
+                .WithMany(x => x.thalassemiaPatientDonorAssignements)
+                .HasForeignKey(x => x.DonarId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
