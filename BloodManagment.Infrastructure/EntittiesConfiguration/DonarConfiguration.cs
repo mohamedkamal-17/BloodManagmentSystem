4f5b683edd9305f81ae631397e8df93c0bc16d41
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:12:07 +0200

    Configur  Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/DonarConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class DonarConfiguration : IEntityTypeConfiguration<Donar>
+    {
+        public void Configure(EntityTypeBuilder<Donar> builder)
+        {
+            builder.Property(x => x.DonarCode).HasMaxLength(50).IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.thalassemiaPatientDonorAssignements)
+                .WithOne(x => x.Donar)
+                .HasForeignKey(x => x.DonarId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+
+}
