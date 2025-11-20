01c43c47f6ecf713d9643e800ea85814be4465f9
Mohamed kamal <winc84327@gmail.com>
2025-11-21 01:11:48 +0200

    Configur  BloodUnit Entity

---
+++ BloodManagment.Infrastructure/EntittiesConfiguration/BloodUnitConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using BloodManagment.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodManagment.Infrastructure.EntittiesConfiguration
+{
+    public class BloodUnitConfiguration : IEntityTypeConfiguration<BloodUnit>
+    {
+        public void Configure(EntityTypeBuilder<BloodUnit> builder)
+        {
+            // Primary Key
+            builder.HasKey(x => x.Id);
+
+            // Properties
+            builder.Property(x => x.StorageLocation)
+                   .HasMaxLength(200)
+                   .IsRequired();
+
+            builder.Property(x => x.BloodGroup)
+                   .HasConversion<string>() // Store enum as string
+                   .IsRequired();
+
+            builder.Property(x => x.Status)
+                   .HasConversion<string>() // Store enum as string
+                   .IsRequired();
+
+            builder.Property(x => x.DonationDate)
+                   .IsRequired();
+
+            builder.Property(x => x.ExpiryDate)
+                   .IsRequired();
+
+            builder.HasOne(x => x.BloodInventory)
+           .WithMany(x => x.BloodUnits)
+           .HasForeignKey(x => x.BloodInventoryId)
+           .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Property(x => x.CreatedAt)
+                   .HasDefaultValueSql("GETUTCDATE()");
+
+
+        }
+    }
+}
