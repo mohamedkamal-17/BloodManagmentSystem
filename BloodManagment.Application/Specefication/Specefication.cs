a868a30d1e9d6a7f9c1584cb6dba832864743eed
Mohamed kamal <winc84327@gmail.com>
2025-11-22 00:37:32 +0200

    Addd specefication pattern

---
+++ BloodManagment.Application/Specefication/Specefication.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+namespace BloodManagment.Application.Specefication
+{
+    public abstract class Specefication<TEntity> where TEntity : class
+    {
+        public Specefication(Expression<Func<TEntity, bool>> criatria)
+        {
+            this.Criatria = criatria;
+
+        }
+
+
+        public Expression<Func<TEntity, bool>>? Criatria { get; }
+
+        public List<Expression<Func<TEntity, object>>>? Includes { get; private set; } = new();
+
+        public Expression<Func<TEntity, object>>? OrderByExpression { get; private set; }
+
+        public Expression<Func<TEntity, object>>? OrderByDescendingExpression { get; private set; }
+
+        public bool IsSplitQuery { get; set; }
+
+        private void AddIncluds(Expression<Func<TEntity, object>> includeExpression)
+                                                                        => Includes.Add(includeExpression);
+
+        private void AddOrderByExpressions(Expression<Func<TEntity, object>> orderByExpression)
+                                                                        => OrderByExpression = orderByExpression;
+        private void AddOrderByDescendingExpression(Expression<Func<TEntity, object>> orderByDescendingExpression)
+                                                                        => OrderByDescendingExpression = orderByDescendingExpression;
+    }
+}
---
+++ BloodManagment.Infrastructure/SpeceficationEvaluater/SpeceficationEvaluater.cs
@@ -0,0 +1,44 @@
+﻿using BloodManagment.Application.Specefication;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodManagment.Infrastructure.SpeceficationEvaluater
+{
+    internal static class SpeceficationEvaluater
+    {
+        internal static IQueryable<T> GetQuery<T>(IQueryable<T> inputQuery, Specefication<T> specefication) where T : class
+        {
+            var query = inputQuery;
+
+            // Apply criteria
+            if (specefication.Criatria is not null)
+            {
+                query = query.Where(specefication.Criatria);
+            }
+
+            // Apply includes
+            query = specefication?.Includes?.Aggregate(query, (crunnt, includeExpression) => crunnt.Include(includeExpression));
+
+
+
+            // Apply ordering
+
+            if (specefication.OrderByExpression is not null)
+            {
+                query = query.OrderBy(specefication.OrderByExpression);
+            }
+            else if (specefication.OrderByDescendingExpression is not null)
+            {
+                query = query.OrderByDescending(specefication.OrderByDescendingExpression);
+            }
+
+            // Apply split query if needed
+            if (specefication.IsSplitQuery)
+            {
+                query = query.AsSplitQuery();
+            }
+
+            return query;
+        }
+    }
+}
