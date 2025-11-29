7559d4170f7504ddfb34ff2f61e66b5fc9b14d6d
Mohamed kamal <winc84327@gmail.com>
2025-11-29 15:18:21 +0200

    CreateGeneric Repo interface

---
+++ BloodManagment.domain/Contracts/Repositorise/IGenirecRepo.cs
@@ -0,0 +1,14 @@
+﻿namespace BloodManagment.domain.Contracts.Reposetorise
+{
+    public interface IGenirecRepo<T> where T : class
+    {
+        public Task AddAsync(T entity);
+
+        public void DeleteAsync(T entity);
+
+        public void UpdateAsync(T entity);
+
+
+
+    }
+}
