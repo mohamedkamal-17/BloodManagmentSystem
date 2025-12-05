e3b591f6b446022ad63aa2cf6528aacb3a2ef6fd
Mohamed kamal <winc84327@gmail.com>
2025-12-06 01:12:13 +0200

    Add custom DatabaseException class for DB error handling
    Added DatabaseException in BloodManagment.Infrastructure.Comman namespace.
    This class inherits from Exception and provides a constructor for error messages, enabling more specific handling of database-related errors.

---
+++ BloodManagment.Infrastructure/Comman/DatabaseException.cs
@@ -0,0 +1,11 @@
+﻿namespace BloodManagment.Infrastructure.Comman
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException(string message
+            ) : base(message)
+        {
+        }
+
+    }
+}
