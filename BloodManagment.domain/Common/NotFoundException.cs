49f8d506d24b3139a8c2ea34ce871ecebe6de787
Mohamed kamal <winc84327@gmail.com>
2025-12-06 01:12:03 +0200

    Add NotFoundException custom exception class
    Added NotFoundException.cs in BloodManagment.Application.Commane namespace. This class inherits from Exception and provides a constructor for custom error messages.

---
+++ BloodManagment.domain/Common/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace BloodManagment.Application.Commane
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message
+            ) : base(message)
+        {
+        }
+    }
+}
