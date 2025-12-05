413f2ba597d9ae71c6ed653d80eb61b7612b227e
Mohamed kamal <winc84327@gmail.com>
2025-12-06 01:11:39 +0200

    Add custom ValidationException class for detailed errors
    Added ValidationException.cs in BloodManagment.Application.Commane namespace. This class extends Exception and introduces an Errors property to capture validation error details as a dictionary. The constructor initializes both the error message and the error dictionary, enabling more informative validation error handling.

---
+++ BloodManagment.Application/Commane/ValidationException.cs
@@ -0,0 +1,16 @@
+﻿namespace BloodManagment.Application.Commane
+{
+    public class ValidationException : Exception
+    {
+
+        public Dictionary<string, string[]> Errors;
+        public ValidationException(string message,
+            Dictionary<string, string[]> errors
+            ) : base(message)
+        {
+            Errors = errors;
+        }
+
+
+    }
+}
