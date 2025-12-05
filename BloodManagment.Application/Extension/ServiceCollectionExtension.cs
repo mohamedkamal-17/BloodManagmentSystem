d80ecb7f45d3e8123d44e8f0608e7adffe0603c5
Mohamed kamal <winc84327@gmail.com>
2025-12-06 00:24:29 +0200

    Add ServiceCollectionExtension for app service registration
    Introduced ServiceCollectionExtension.cs with a static class and extension method in BloodManagment.Application.Extension. This prepares a structure for future application-level service registrations without altering IServiceCollection behavior yet.

---
+++ BloodManagment.Application/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BloodManagment.Application.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection ApplicationServiceCollectionExtension(
+            this IServiceCollection services
+            )
+        {
+
+            return services;
+        }
+    }
+}
