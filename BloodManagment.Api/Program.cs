2575226d31cb37c325095c1b44207bd1e60ce53b
Mohamed kamal <winc84327@gmail.com>
2025-12-06 00:26:48 +0200

    Centralize service config and add JWT authentication setup
    Refactored Program.cs to use InfrastructureServiceCollectionExtension for service registration, centralizing DbContext and Identity configuration. Updated appsettings.json with JwtSettings for JWT authentication. Added Microsoft.AspNetCore.Authentication.JwtBearer package reference to BloodManagment.Infrastructure.csproj.

--- BloodManagment.Api/Program.cs
+++ BloodManagment.Api/Program.cs
@@ -1,8 +1,4 @@
-
-using BloodManagment.domain.Entities;
-using BloodManagment.Infrastructure.DataHelper;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using BloodManagment.Infrastructure.Extension;
 
 namespace BloodManagment.Api
 {
@@ -13,24 +9,8 @@ namespace BloodManagment.Api
             var builder = WebApplication.CreateBuilder(args);
 
 
-            builder.Services.AddDbContext<ApplicationContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("cs"));
-            });
+            builder.Services.InfrastructureServiceCollectionExtension(builder.Configuration);
 
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                options.User.AllowedUserNameCharacters =
-                     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._ ";
-                options.User.RequireUniqueEmail = true;
-                options.Password.RequireDigit = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequiredLength = 4;
-            })
-            .AddEntityFrameworkStores<ApplicationContext>()
-            .AddDefaultTokenProviders();
             // Add services to the container.
             builder.Services.AddAuthorization();
 
