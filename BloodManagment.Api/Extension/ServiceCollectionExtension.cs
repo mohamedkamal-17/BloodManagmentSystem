1cfba50cd10cdc77c3e160ce8c2913d16e7e8328
Mohamed kamal <winc84327@gmail.com>
2025-12-06 01:09:18 +0200

    Add ServiceCollectionExtension for DI and exception handling
    Introduce ServiceCollectionExtension.cs with an extension method to register GlobalExceptionHandler and ProblemDetails support, streamlining presentation-layer service configuration in dependency injection.

---
+++ BloodManagment.Api/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using BloodManagment.Api.Comman;
+
+namespace BloodManagment.Api.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection PresintationServiceCollectionExtension(this IServiceCollection services)
+        {
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+            services.AddProblemDetails();
+            return services;
+        }
+    }
+}
